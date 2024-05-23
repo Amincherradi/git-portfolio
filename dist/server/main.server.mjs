c85ca91eafd021137546e09e54431c9b301d033f
Amine Cherradi <a.cherradi@digi4.ma>
2024-05-23 18:21:52 +0100

    deploy

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OGP3SO3Y.mjs";import"./chunk-UEYX5QTM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2CT3A6IO.mjs";import"./chunk-UEYX5QTM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
